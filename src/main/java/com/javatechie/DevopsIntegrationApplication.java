1784d35b6954c00ddd35e4b3f38adbbada81b436
prahashp <93474423+prahashp@users.noreply.github.com>
2023-08-10 17:01:11 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "VANAKKAM DA MAPALA TANAGOEYE";
+		return "WELCOME TO TANAGOEYE";
 	}
 
 	public static void main(String[] args) {
